890919a931d5c38644845545c527a2116679cbd6
habitmaster <62680650+habitmaster@users.noreply.github.com>
2022-12-24 17:28:28 +0800

    Add files via upload
    Working version - formatting to be cleaned up.

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 
 const output = document.querySelector('.output');
-const id = '1NF43Fp18YOp79M1B1tqY-Im3Isx4g-iEQPoc3dcYzyo';
+const id = '1CmQGM1ik9naPVDTLoBaBKC1AzopZdEWDc48REaFSmyk';
 const base = 'https://docs.google.com/spreadsheets/d/';
-const query = 'SELECT *';
-const tq = '/gviz/tq?tqx=out:json&tq=';
-const endPoint = `${base}${id}${tq}`;
+const q1 = '/gviz/tq?';
+const q2 = 'tqx=out:json';
+const q3 = 'sheet=sheet1';
+const endPoint = `${base}${id}${q1}&${q2}&${q3}`;
 
 fetch(endPoint).then(res=>res.text())
 .then(data => {
@@ -12,21 +13,47 @@ let temp = data.replace('/*O_o*/','');
 temp = temp.replace('google.visualization.Query.setResponse(','');
 temp = temp.replace(');','');
 const jsonData = JSON.parse(temp);
+console.log(jsonData.table); 
+const headings = makeCell(output, '', 'heading');
+jsonData.table.cols.forEach((col)=>{
+ console.log(col);
+    const el = makeCell(headings, col.label, 'box') 
+})
+jsonData.table.rows.forEach((row)=>{
+    console.log(row);
+    const div = makeCell(output, '','row');
+    row.c.forEach((cell)=>{
+    const ele1 = makeCell(div,`${cell.v}`,'box');
+});
+})
 
 
-const rows = jsonData.table.rows;
-rows.forEach((row) =>{
-  const div = document.createElement("div");
-    const temp1 = row.c;
-    temp1.forEach((cell)=>{
-      const box = document.createElement("div");
-        box.textContent = cell.v;
-        box.classList.add("box");
-        div.append(box);
-    })
-    output.append(div);
-    });
 });
 
-getData();
+function makeCell(parent, html, classAdd){
+    const ele = document.createElement('div');
+    parent.append(ele);
+    ele.innerHTML = html; 
+    ele.classList.add(classAdd);
+       return ele;
+}
+
+// cell.v
+
+
+// const rows = jsonData.table.rows;
+// rows.forEach((row) =>{
+//   const div = document.createElement("div");
+//     const temp1 = row.c;
+//     temp1.forEach((cell)=>{
+//       const box = document.createElement("div");
+//         box.textContent = cell.v;
+//         box.classList.add("box");
+//         div.append(box);
+//     })
+//     output.append(div);
+//     });
+// });
+
+
 
